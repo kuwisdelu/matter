256abcd8b4b6cd150879748cd46c55dd548e0827
kuwisdelu <k.bemis@northeastern.edu>
2019-10-13 03:00:47 -0700

    more rowStats/colStats docs

--- tests/testthat/test-col-stats.R
+++ tests/testthat/test-col-stats.R
@@ -100,3 +100,51 @@ test_that("col-stats-multi", {
 
 })
 
+test_that("col-stats-multi", {
+
+	register(SerialParam())
+
+	set.seed(1)
+
+	dmn <- list(paste0("row", 1:100), paste0("col", 1:100))
+
+	x <- matrix(runif(100^2), nrow=100, ncol=100, dimnames=dmn)
+
+	groups <- as.factor(rep(letters[1:5], each=20))
+
+	center <- 1:100 - 50.5
+	scale <- sqrt(1:100)
+
+	ans1 <- colStats(x, "sum", col.center=center, col.scale=scale)
+
+	ans2 <- colSums(scale(x, center=center, scale=scale))
+
+	expect_equal(ans1, ans2)
+
+	ans1 <- colStats(x, "sum", row.center=center, row.scale=scale)
+
+	ans2 <- colSums((x - center) / scale)
+
+	expect_equal(ans1, ans2)
+
+	set.seed(2)
+
+	u <- runif(length(center) * nlevels(groups))
+
+	center <- matrix(center + u, ncol=nlevels(groups))
+	scale <- matrix(scale + u, ncol=nlevels(groups))
+
+	colnames(center) <- levels(groups)
+	colnames(scale) <- levels(groups)
+
+	ans1 <- colStats(x, "sum", groups=groups, col.center=center)
+
+	ans2 <- sapply(levels(groups), function(g) {
+		xc <- x - t(center[,groups])
+		colSums(xc[g==groups,,drop=FALSE])
+	})
+
+	expect_equal(ans1, ans2)
+
+})
+
