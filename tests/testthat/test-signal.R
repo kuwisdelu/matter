5f7bf5062b321f4bf49bb5fe0c34b6af17696e1d
Kylie Bemis <k.bemis@northeastern.edu>
2023-05-12 12:26:27 -0400

    added unit test for peak prominence

--- tests/testthat/test-signal.R
+++ tests/testthat/test-signal.R
@@ -49,28 +49,34 @@ test_that("findpeaks", {
 	x <- c(0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 2, 2, 3, 0, 1)
 	names(x) <- seq_along(x)
 	p1 <- findpeaks(x)
-	l1 <- attr(p1, "left_bounds")
-	r1 <- attr(p1, "right_bounds")
 
 	expect_equivalent(p1, c(4, 7, 13))
-	expect_equal(l1, c(3, 6, 10))
-	expect_equal(r1, c(5, 10, 14))
+	expect_equal(attr(p1, "left_bounds"), c(3, 6, 10))
+	expect_equal(attr(p1, "right_bounds"), c(5, 10, 14))
 
 	x[11] <- 3
 	p2 <- findpeaks(x)
-	l2 <- attr(p2, "left_bounds")
-	r2 <- attr(p2, "right_bounds")
 
 	expect_equivalent(p2, c(4, 7, 11))
-	expect_equal(l2, c(3, 6, 10))
-	expect_equal(r2, c(5, 10, 14))
+	expect_equal(attr(p2, "left_bounds"), c(3, 6, 10))
+	expect_equal(attr(p2, "right_bounds"), c(5, 10, 14))
 
 	x <- c(0, 0, 0, 0, 1, 0, 0, 0, 0, 0)
+
 	p3 <- findpeaks(x)
 
 	expect_equivalent(p3, 5)
 	expect_equal(attr(p3, "left_bounds"), 4)
 	expect_equal(attr(p3, "right_bounds"), 6)
+
+	x <- c(0, 1, 0, 3, 1, 3, 0, 4, 0)
+	names(x) <- seq_along(x)
+	p4 <- findpeaks(x, prominence=TRUE)
+
+	expect_equivalent(p4, c(4, 8))
+	expect_equal(attr(p4, "left_bases"), c(3, 7))
+	expect_equal(attr(p4, "right_bases"), c(7, 9))
+	expect_equal(attr(p4, "prominence"), c(3, 4))
 
 })
 
