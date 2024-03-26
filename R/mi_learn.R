1b7c35a9402e1fe8a05312eae720a044e84a9fb9
Kylie Bemis <k.bemis@northeastern.edu>
2024-03-25 22:10:10 -0400

    mi_learn()

---
+++ R/mi_learn.R
@@ -0,0 +1,72 @@
+
+#### Multiple instance learning ####
+## ---------------------------------
+
+mi_learn <- function(fn, x, y, group,
+	threshold = 0.01, verbose = NA, ...)
+{
+	if ( is.na(verbose) )
+		verbose <- getOption("matter.default.verbose")
+	y <- as.factor(y)
+	if ( nlevels(y) != 2L )
+		stop("y must have exactly 2 levels")
+	pos <- levels(y)[1L]
+	neg <- levels(y)[2L]
+	if ( verbose )
+		message("using ", sQuote(pos), " as positive class")
+	group <- as.factor(group)
+	if ( length(y) != length(group) ) {
+		if ( length(y) != nlevels(group) ) {
+			stop("length of y [", length(y), "] does not match ",
+				"length of group [", length(group), "] ",
+				"or its levels [", nlevels(group), "]")
+		} else {
+			yg <- y
+			y <- y[as.integer(group)]
+		}
+	} else {
+		y <- rep_len(y, length(group))
+		yg <- lapply(levels(group), function(g) unique(y[group %in% g]))
+		if ( any(lengths(yg) != 1L) ) {
+			stop("labels must be homogenous within each group")
+		} else {
+			yg <- unlist(yg)
+		}
+	}
+	iter <- 1
+	uprop <- 1
+	# multiple instance learning
+	while ( uprop > threshold )
+	{
+		if ( verbose )
+			message("multiple instance iteration ", iter)
+		model <- fn(x, y, ...)
+		yi <- fitted(model, "class")
+		py <- fitted(model, "probability")[,1L]
+		for ( j in seq_along(yg) )
+		{
+			g <- levels(group)[j]
+			# set all negative bag labels to neg
+			if ( all(yg %in% neg) )
+			{
+				yi[group %in% g] <- neg
+				next
+			}
+			# update positive bag labels
+			yj <- yi[group %in% g]
+			if ( all(yj %in% neg) ) {
+				imax <- which.max(py[group %in% g])
+				yj[imax] <- pos
+			}
+			yi[group %in% g] <- yj
+		}
+		iter <- iter + 1
+		utot <- sum(y != yi)
+		uprop <- utot / length(y)
+		if ( verbose )
+			message(utot, " labels updated (", 100 * uprop, "%)")
+		y <- yi
+	}
+	fn(x, y, ...)
+}
+
---
+++ tests/testthat/test-mi_learn.R
@@ -0,0 +1,52 @@
+require(testthat)
+require(matter)
+
+context("mi_learn")
+
+test_that("mi_learn", {
+
+	register(SerialParam())
+	set.seed(1)
+	n <- 100
+	p <- 5
+	g <- 5
+	group <- rep(paste0("s", seq_len(g)), each=n %/% g)
+	group <- factor(rep_len(group, n))
+	x <- matrix(rnorm(n * p), nrow=n, ncol=p)
+	colnames(x) <- paste0("x", seq_len(p))
+
+	# create bagged labels
+	y <- ifelse(group %in% c("s1", "s2", "s3"), "pos", "neg")
+	y <- factor(y, levels=c("pos", "neg"))
+	ipos <- y %in% "pos"
+	ineg <- y %in% "neg"
+	z <- y
+
+	# create "true" labels (with some within-bag noise)
+	z[ipos] <- sample(c("pos", "neg"), sum(ipos), replace=TRUE)
+	jpos <- z %in% "pos"
+	jneg <- z %in% "neg"
+
+	# create data
+	x[jpos,] <- x[jpos,] + rnorm(p * sum(jpos), mean=1)
+	x[jneg,] <- x[jneg,] - rnorm(p * sum(jneg), mean=1)
+
+	# create "observed" labels
+	yj <- c("pos", "pos", "pos", "neg", "neg")
+	yj <- factor(yj, levels=c("pos", "neg"))
+
+	fit0 <- nscentroids(x=x, y=y)
+	fit1 <- mi_learn(nscentroids, x=x, y=y, group=group)
+	fit2 <- mi_learn(nscentroids, x=x, y=yj, group=group)
+
+	y0 <- fitted(fit0, "class")
+	y1 <- fitted(fit1, "class")
+	y2 <- fitted(fit2, "class")
+
+	expect_equal(class(fit0), class(fit1))
+	expect_equal(fit1, fit2)
+	expect_equal(y1, y2)
+	expect_gt(mean(y1 == z), mean(y0 == z))
+	expect_gt(mean(y2 == z), mean(y0 == z))
+
+})
